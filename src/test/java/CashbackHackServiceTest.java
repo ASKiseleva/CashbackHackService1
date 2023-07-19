452f6e59aa70ab89a815ffe4b808daacf2ce1b55
Neto test <anna.shuliko@bk.ru>
2023-07-19 13:22:32 +0300

    Add Junit4

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -1,11 +1,11 @@
-import static org.testng.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 import ru.netology.CashbackHackService;
 
 import java.util.concurrent.Callable;
 
 public class CashbackHackServiceTest {
-    @org.testng.annotations.Test
+    @org.junit.Test
 
     public void testRemain() {
         CashbackHackService service = new CashbackHackService();
@@ -14,7 +14,7 @@ public class CashbackHackServiceTest {
         int expected = 0;
         assertEquals(expected, actual);
     }
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testRemainWithCashback() {
         CashbackHackService service = new CashbackHackService();
         int amount = 900;
